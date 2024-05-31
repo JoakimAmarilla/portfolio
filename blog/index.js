b7e3c847ebc2b0d1fcf3f1a2da24c42a410ad241
JoakimAmarilla <joa.al019@hotmail.com>
2024-05-31 11:34:46 -0300

    Header 90percent ready

--- blog/index.js
+++ blog/index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res)=>{
-    res.render("home.ejs")
+    res.render("home.ejs",{date: "05-21-24"})
 })
 
 app.listen(port, ()=>{
